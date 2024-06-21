483839227e4549b7b6e9465687fc306ce8a4ff76
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2024-06-21 10:53:06 -0300

    Se agrega función Middleware para conversation schema

--- model/conversation.js
+++ model/conversation.js
@@ -20,4 +20,38 @@ const ConversationSchema = new mongoose.Schema({
     messages: []
 })
 
+
+ConversationSchema.pre('save', function(next) {
+    const document = this
+    
+    // La conversación también se guarda al actualizar feedback de mensajes antigüos
+    // Podría hacer un save de una conversación cerrada
+    if(document.status === "open"){
+        console.log("La conversación está abierta")
+        setTimeout(()=>{
+            const MAX_MINUTES = 1
+            const lastMessage = document.messages[document.messages.length - 1]
+            
+            let last = (parseInt(lastMessage.date))
+            let current = Date.now()
+
+            const diff = Math.abs(current - last)
+            const minutes = diff / (1000 * 60)
+
+            console.log(minutes > MAX_MINUTES)
+            if(minutes > MAX_MINUTES){
+                console.log("Hay que cerrar la conversación")
+            }
+            
+        }, 120000)
+
+    } else {
+        console.log("La conversación está cerrada")
+    }
+    
+
+    next()
+})
+
+
 export default mongoose.model('Conversation', ConversationSchema)
